062ff82e15df4b606e5c8af522ac8e63ecb83ab8
Paula007 <paula@videvik.com>
2020-12-08 17:32:45 +0200

    Change to HttpUrlConnection

--- CryptoBack/src/main/java/ee/videvik/CryptoBack/service/TickerValueService.java
+++ CryptoBack/src/main/java/ee/videvik/CryptoBack/service/TickerValueService.java
@@ -1,50 +1,49 @@
 package ee.videvik.CryptoBack.service;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.client.RestTemplate;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-
-import static com.google.gson.JsonParser.parseReader;
-
 
 public class TickerValueService {
 
-    // api connection setup with java.urlConnection
-    public static String connectUrl(String url) throws IOException {
-        JsonObject obj = new JsonObject();
-        URL connectTo = new URL(url);
-        URLConnection request = connectTo.openConnection();
-        request.connect();
-
-        try {
-            JsonElement element = parseReader(new InputStreamReader((InputStream) request.getContent()));
-            obj = element.getAsJsonObject();
-        } catch (Exception e) {
-            obj.addProperty("default", "Something went wrong");
-        }
-
-        return obj.toString();
-    }
-
     // EUR rate query for Ripple
     public static double getEurRate() throws IOException {
-        String url = "https://www.freeforexapi.com/api/live?pairs=USDEUR";
-        double eurRate = Double.parseDouble(connectUrl(url).replaceAll(".*?([\\d.]+).*",
+        URL url = new URL("https://www.freeforexapi.com/api/live?pairs=USDEUR");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setReadTimeout(10000);
+
+        BufferedReader inputReader = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+
+        double eurRate = Double.parseDouble(inputReader.readLine().replaceAll(".*?([\\d.]+).*",
                 "$1"));
+        System.out.println(eurRate);
+
+        inputReader.close();
 
         return eurRate;
     }
 
+    // rounding the crypto price to 2 decimal points
+    private static double round(double value) {
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
     // retrieve latest price for cryptocurrencies from Bitfinex api
     public static double getMarketValue(int amount, String currency) throws IOException {
+
+        double marketValue = 0;
         String symbol = "";
+
+        // assign ticker symbol to crypto
         switch (currency) {
             case ("Bitcoin"):
                 symbol = "tBTCEUR";
@@ -58,23 +57,47 @@ public class TickerValueService {
             default:
                 System.out.println("Currency not found");
         }
-        // here the api call receives 403 forbidden [error 1010], meaning
-        // The owner of this website has banned access based on your browser's signature
-        String uri = "https://api-pub.bitfinex.com/v2/ticker/" + symbol;
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("user-agent",
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
-                        "(KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36");
-        headers.add("Accept", "*/*");
-        String[] cryptoTicker = restTemplate.getForObject(uri, String[].class, headers);
-        double lastPrice = Double.parseDouble(cryptoTicker[6]);
-        if (currency.equals("Ripple")) {
-            lastPrice = lastPrice * TickerValueService.getEurRate();
-        }
-        double marketValue = amount * lastPrice;
 
-        return marketValue;
+            // connect to Bitfinex API
+        try {
+            URL url = new URL("https://api-pub.bitfinex.com/v2/ticker/" + symbol);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                    "(KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36");
+            connection.setRequestProperty("Accept", "application/json, text/plain, */*");
+            connection.setReadTimeout(10000);
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if (responseCode != 200) {
+                System.out.println("Error reading API");
+            }
+
+            // read the last price, value of 6th index in response array
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String response = inputReader.readLine();
+            String[] cryptoValue = response.split(",");
+            double lastPrice = Double.parseDouble(cryptoValue[6]);
+
+            // calculate EUR value for Ripple since Bitfinex doesn't give any
+            if (currency.equals("Ripple")) {
+                lastPrice = lastPrice * TickerValueService.getEurRate();
+            }
+
+            marketValue = amount * lastPrice;
+
+            inputReader.close();
+
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+        return round(marketValue);
     }
 
 }
